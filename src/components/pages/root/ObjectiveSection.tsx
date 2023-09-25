6ba1385414a1a0b29accf6fe0b11021a7009a7ab
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-09-25 16:28:16 +0200

    fix(OjectiveSection.tsx) import

--- src/components/pages/root/ObjectiveSection.tsx
+++ src/components/pages/root/ObjectiveSection.tsx
@@ -1,7 +1,5 @@
+import { h2Styles, sectionStyles } from "../../../routes/root";
 import { ObjectiveCard } from "./ObjectiveCard";
-
-import { h2Styles } from "../../../routes/root";
-import { sectionStyles } from "../../../routes/root";
 
 export const ObjectiveSection = () => {
   return (
@@ -10,35 +8,35 @@ export const ObjectiveSection = () => {
 
       <div className="my-8 grid gap-12 text-center lg:grid-cols-2">
         <ObjectiveCard
-          cardTitle="Éduquer et sensibiliser"
           cardContent="Chacun de ces personas est conçu pour vous sensibiliser aux défis
         spécifiques auxquels sont confrontés certains utilisateurs en ligne.
         Vous découvrirez les obstacles qui limitent l’accès à l’information, à
         la communication et aux services pour de nombreuses personnes."
+          cardTitle="Éduquer et sensibiliser"
         />
 
         <ObjectiveCard
-          cardTitle="Contribuez à l’inclusion numérique"
           cardContent="Notre objectif est de promouvoir l’inclusion numérique en
         sensibilisant à ces défis. Vous pouvez également trouver des
         ressources et des conseils pour améliorer l’accessibilité de votre
         propre site web et contribuer à un Internet plus inclusif."
+          cardTitle="Contribuez à l’inclusion numérique"
         />
 
         <ObjectiveCard
-          cardTitle="Changez de perspective"
           cardContent="Sur Inotest, vous pouvez passer d’un persona à une autre, ce qui vous
         permet de comprendre les défis auxquels sont confrontées différentes
         personnes en ligne."
+          cardTitle="Changez de perspective"
         />
 
         <ObjectiveCard
-          cardTitle="Rejoignez Notre communauté d’inclusion"
           cardContent="Ensemble, nous pouvons construire un web plus accessible et inclusif
         pour tous. L’inclusion numérique est un voyage collectif. Venez
         explorer le web à travers de nouvelles perspectives et contribuez à
         faire du web un espace plus accessible pour tous les internautes.
         Rejoignez-nous maintenant !"
+          cardTitle="Rejoignez Notre communauté d’inclusion"
         />
       </div>
     </section>
