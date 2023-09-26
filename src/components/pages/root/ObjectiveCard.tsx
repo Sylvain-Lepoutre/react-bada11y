af025935352cada31a5e4c73c1119648512c711f
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-09-26 10:16:25 +0200

    feat(ObjectiveCard.tsx): style

--- src/components/pages/root/ObjectiveCard.tsx
+++ src/components/pages/root/ObjectiveCard.tsx
@@ -4,7 +4,7 @@ type Props = { cardTitle: string; cardContent: string };
 
 export const ObjectiveCard = ({ cardTitle, cardContent }: Props) => {
   return (
-    <article className="mx-auto flex w-9/12 max-w-lg list-none flex-col justify-center rounded-2xl p-4 shadow-Custom outline outline-2 outline-BluePastel">
+    <article className="mx-auto flex w-9/12 max-w-lg list-none flex-col justify-center rounded-2xl bg-gradient-to-br from-BluePastel to-BlueAqua p-4 shadow-Custom outline outline-2 outline-BluePastel">
       <h3 className="text-center text-xl">{cardTitle}</h3>
       <p className={pStyles}>{cardContent}</p>
     </article>
