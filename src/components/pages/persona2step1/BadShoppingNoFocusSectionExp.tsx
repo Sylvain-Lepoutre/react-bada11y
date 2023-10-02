09141ff43548299856b6605f3cdb68eafb84fc44
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-10-02 15:13:44 +0200

    feat(BadShoppingNoFocusSectionExp.tsx): create component

---
+++ src/components/pages/persona2step1/BadShoppingNoFocusSectionExp.tsx
@@ -0,0 +1,25 @@
+import { Heading2 } from "../../headings/Heading2";
+import { Section } from "../../sections/Section";
+
+import { pStyles } from "../../../routes/root";
+
+export const BadShoppingNoFocusSectionExp = () => {
+  return (
+    <Section>
+      <Heading2>Explication</Heading2>
+      <p className={pStyles}>
+        Comme nous l’avons vu, Sophie doit utiliser son clavier pour naviguer,
+        elle utilise la touche Tab pour passer de champ interactif en champ
+        interactif. Le soucis dans notre cas présent, c’est qu’il y a aucune
+        indication visuelle pour informer où nous nous trouvons dans notre
+        parcours sur les différents produits.
+      </p>
+      <p className={pStyles}>
+        Tout nos champs interactifs doivent avoir un style visuel qui est
+        modifié lorsque l’élément possède le focus afin que l’utilisateur sache
+        toujours où il es lorsqu’il navigue au clavier, ou techonologies
+        d’assistance simulant l’utilisation d’un clavier.
+      </p>
+    </Section>
+  );
+};
