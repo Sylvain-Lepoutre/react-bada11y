2fe36ad7b0bea3bf8392f571eb8d51ca5a004162
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-10-02 09:00:28 +0200

    fix(personaHome.tsx): clear static text

--- src/routes/personaHome.tsx
+++ src/routes/personaHome.tsx
@@ -19,12 +19,6 @@ export const PersonaHome = ({
       <main className="mx-auto my-12 w-11/12">
         <IntroSection introContent={personaIntro} />
 
-        <Section>
-          Djebrine est en train de préparer une présentation pour ses employés,
-          il navigue sur diverses pages web à la recherche d’informations.
-          Soudain son flux d’informations se trouve interrompu.
-        </Section>
-
         <DivLink
           labelLink1="Retour"
           labelLink2="Commencer"
