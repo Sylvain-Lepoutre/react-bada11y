a2e3015c60e0ff559d4fcd33c357f8d87f7b81fe
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-10-02 11:39:40 +0200

    feat(BadShoppingNoFocusSection.tsx) create component

--- src/routes/personaStep1.tsx
+++ src/routes/personaStep1.tsx
@@ -1,7 +1,7 @@
 import { Header } from "../components/headers/Header";
 import { BadFormNoSemanticSection } from "../components/pages/persona1step1/BadFormNoSemanticSection";
 import { BadFormNoSemanticSectionSR } from "../components/pages/persona1step1/BadFormNoSemanticSectionSR";
-import { BadFormNoFocusSection } from "../components/pages/persona2step1/BadFormNoFocusSection";
+import { BadShoppingNoFocusSection } from "../components/pages/persona2step1/BadShoppingNoFocusSection";
 import { DivLink } from "../components/pages/personaHome/DivLink";
 import { useDocumentTitle } from "../hooks/useDocumentTitle";
 
@@ -14,7 +14,7 @@ export const PersonaStep1 = ({ personaName, personaStep1Title }: Props) => {
       <Header heading={personaName} textContent={personaStep1Title} />
       <main className="mx-auto my-12 w-11/12">
         {personaName === "Djebrine" ? <BadFormNoSemanticSection /> : null}
-        {personaName === "Sophie" ? <BadFormNoFocusSection /> : null}
+        {personaName === "Sophie" ? <BadShoppingNoFocusSection /> : null}
 
         {personaName === "Djebrine" ? <BadFormNoSemanticSectionSR /> : null}
         {personaName === "Sophie" ? <h1>Work in Progress</h1> : null}
