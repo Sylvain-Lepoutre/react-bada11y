57b20724499fc5d2b58aba59f2879e8194e932e0
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-10-03 14:06:43 +0200

    feat(BadFormNoSemanticCode.tsx): create component

--- src/components/pages/persona1step1/BadFormNoSemanticCode.tsx
+++ src/components/pages/persona1step1/BadFormNoSemanticCode.tsx
@@ -1,46 +1,18 @@
+import { atomOneDark, CodeBlock } from "react-code-blocks";
+
+import { codes } from "../../../data/codes";
+import { Heading2 } from "../../headings/Heading2";
 import { Section } from "../../sections/Section";
 
 export const BadFormNoSemanticCode = () => {
   return (
     <Section>
-      <pre>
-        <code>
-          <div className="mx-auto my-4 w-3/4 rounded-lg bg-gray-200 p-4">
-            <div className="text-center">Inscription</div>
-
-            <div className="m-4 flex flex-col">
-              <span>Nom</span>
-              <input className="rounded-lg px-2" type="text" />
-            </div>
-
-            <div className="m-4 flex flex-col">
-              <span>Prénom</span>
-              <input className="rounded-lg px-2" type="text" />
-            </div>
-
-            <div className="m-4 flex flex-col">
-              <span>E-mail</span>
-              <input className="rounded-lg px-2" type="email" />
-            </div>
-
-            <div className="m-4 flex flex-col">
-              <span>Mot de passe</span>
-              <input className="rounded-lg px-2" type="password" />
-            </div>
-
-            <div className="m-4 flex flex-col">
-              <span>Vérification du Mot de passe</span>
-              <input className="rounded-lg px-2" type="password" />
-            </div>
-
-            <input
-              className="mx-auto block rounded-lg bg-gray-700 p-2 text-white"
-              type="submit"
-              value="Soumettre l'inscription"
-            />
-          </div>
-        </code>
-      </pre>
+      <Heading2>Code</Heading2>
+      <CodeBlock
+        language="html"
+        text={codes.BadFormNoSemantic}
+        theme={atomOneDark}
+      />
     </Section>
   );
 };
