ca17881b581bfbae6edfaf718c5a39ae84c5346e
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-10-04 08:42:14 +0200

    fix(codes.tsx): clean code

--- src/data/codes.ts
+++ src/data/codes.ts
@@ -1,36 +1,35 @@
 export const codes = {
-  BadFormNoSemantic: `<div className="mx-auto my-4 w-3/4 rounded-xl bg-gray-200 p-4">
-  <div className="text-center">Inscription</div>
+  BadFormNoSemantic: `<div>
+    <div>Inscription</div>
 
-  <div className="m-4 flex flex-col">
-    <span>Nom</span>
-    <input className="rounded-lg px-2" type="text" />
-  </div>
+    <div>
+        <span>Nom</span>
+        <input type="text" />
+    </div>
 
-  <div className="m-4 flex flex-col">
-    <span>Prénom</span>
-    <input className="rounded-lg px-2" type="text" />
-  </div>
+    <div>
+        <span>Prénom</span>
+        <input type="text" />
+    </div>
 
-  <div className="m-4 flex flex-col">
-    <span>E-mail</span>
-    <input className="rounded-lg px-2" type="email" />
-  </div>
+      <div>
+        <span>E-mail</span>
+        <input type="email" />
+    </div>
 
-  <div className="m-4 flex flex-col">
-    <span>Mot de passe</span>
-    <input className="rounded-lg px-2" type="password" />
-  </div>
+    <div>
+        <span>Mot de passe</span>
+        <input type="password" />
+    </div>
 
-  <div className="m-4 flex flex-col">
-    <span>Vérification du Mot de passe</span>
-    <input className="rounded-lg px-2" type="password" />
-  </div>
+    <div>
+        <span>Vérification du Mot de passe</span>
+        <input type="password" />
+    </div>
 
-  <input
-    className="mx-auto block rounded-lg bg-gray-700 p-2 text-white"
-    type="submit"
-    value="Soumettre l'inscription"
-  />
+    <input>
+      type="submit"
+      value="Soumettre l'inscription"
+    />
 </div>`,
 };
