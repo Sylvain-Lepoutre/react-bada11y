fd8ede3721ecc296dcf34975a9ccc10eedc607f7
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-10-05 11:26:23 +0200

    feat(personaStep3.tsx): add DivLink component

--- src/routes/personaStep3.tsx
+++ src/routes/personaStep3.tsx
@@ -1,5 +1,6 @@
 import { A11yForm } from "../components/pages/persona1step3/A11yForm";
 import { A11FormCode } from "../components/pages/persona1step3/A11yFormCode";
+import { DivLink } from "../components/pages/personaHome/DivLink";
 import { Header } from "../components/UI/headers/Header";
 import { Main } from "../components/UI/main/Main";
 
@@ -21,6 +22,15 @@ export const PersonaStep3 = ({ personaName, personaStep3Title }: Props) => {
             <A11FormCode />
           </>
         )}
+
+        <DivLink
+          labelLink1="Accueil"
+          labelLink2="Continuer"
+          srLabelLink1="Revenir à la page d'accueil de Inotest"
+          srLabelLink2={`Continuer le parcours de ${personaName}`}
+          urlLink1="/"
+          urlLink2={`/wip`}
+        />
       </Main>
     </>
   );
