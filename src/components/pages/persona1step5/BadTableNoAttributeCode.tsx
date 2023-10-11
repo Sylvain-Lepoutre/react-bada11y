2c9771b992f4a974c58581af6029ce52aa663608
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-10-11 11:47:47 +0200

    fix(project): format

--- src/components/pages/persona1step5/BadTableNoAttributeCode.tsx
+++ src/components/pages/persona1step5/BadTableNoAttributeCode.tsx
@@ -1,7 +1,8 @@
-import { CodeBlock, a11yDark } from "react-code-blocks";
+import { a11yDark, CodeBlock } from "react-code-blocks";
+
+import { codes } from "../../../data/codes";
 import { Heading2 } from "../../UI/headings/Heading2";
 import { Section } from "../../UI/sections/Section";
-import { codes } from "../../../data/codes";
 
 export const BadTableNoAttributeCode = () => {
   return (
