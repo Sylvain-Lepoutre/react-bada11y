75a304724e5c24317e694f30505ffd3c398df114
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-10-11 15:58:47 +0200

    fix(project): fix style for contrast

--- src/components/pages/persona1step5/BadTableNoAttribute.tsx
+++ src/components/pages/persona1step5/BadTableNoAttribute.tsx
@@ -5,11 +5,11 @@ export const BadTableNoAttribute = () => {
   return (
     <Section>
       <Heading2>Affichage dans le navigateur</Heading2>
-      <table className="mx-auto my-4 w-3/4 rounded-lg border border-solid border-black">
-        <caption>Evenement à venir</caption>
+      <table className="mx-auto my-4 w-3/4 border-solid border-black">
+        <caption>Évènement à venir</caption>
 
-        <tr className="flex rounded-t-lg bg-gray-700 p-4 text-white">
-          <td className="my-auto w-1/3 text-center">Evenement</td>
+        <tr className="flex rounded-t-lg bg-gray-800 p-4 text-white">
+          <td className="my-auto w-1/3 text-center">Évènement</td>
           <td className="my-auto w-1/3 text-center">Lieu</td>
           <td className="my-auto w-1/3 text-center">Date</td>
         </tr>
@@ -24,7 +24,7 @@ export const BadTableNoAttribute = () => {
           <td className="my-auto w-1/3 text-center">15 mars 2024</td>
         </tr>
 
-        <tr className="flex border-t border-solid border-black bg-gray-500 p-4 text-white">
+        <tr className="flex border-t border-solid border-black bg-gray-600 p-4 text-white">
           <td className="my-auto w-1/3 text-center">
             Conférence sur les tendances du développement web
           </td>
@@ -42,7 +42,7 @@ export const BadTableNoAttribute = () => {
           <td className="my-auto w-1/3 text-center">20 mai 2024</td>
         </tr>
 
-        <tr className="flex rounded-b-lg border-t border-solid border-black bg-gray-500 p-4 text-white">
+        <tr className="flex rounded-b-lg border-t border-solid border-black bg-gray-600 p-4 text-white">
           <td className="my-auto w-1/3 text-center">
             Table ronde sur l’avenir du développement web
           </td>
