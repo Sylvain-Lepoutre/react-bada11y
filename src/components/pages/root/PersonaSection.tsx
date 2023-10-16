2b9d0c10f3cc9539757810746025c5bbe2c481b5
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-10-16 09:27:51 +0200

    feat(project): add TODO

--- src/components/pages/root/PersonaSection.tsx
+++ src/components/pages/root/PersonaSection.tsx
@@ -7,7 +7,7 @@ export const PersonaSection = () => {
   return (
     <Section>
       <Heading2> Choisissez Votre Persona de sensibilisation</Heading2>
-
+      {/* TODO mettre une nav ici ?  */}
       <ul className="my-8 grid gap-12 text-center md:grid-cols-2 lg:my-12  ">
         {personas.map((persona) => (
           <PersonaCard
