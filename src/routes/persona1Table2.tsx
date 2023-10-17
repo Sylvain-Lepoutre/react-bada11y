9823a471c043a94c6b3bbb9dbe7e21efaf35223c
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-10-17 10:32:15 +0200

    feat(A11yTable.tsx): create component

--- src/routes/persona1Table2.tsx
+++ src/routes/persona1Table2.tsx
@@ -1,4 +1,6 @@
 import { Header } from "../components/UI/headers/Header";
+import { Main } from "../components/UI/main/Main";
+import { A11yTable } from "../components/pages/persona1/persona1table2/A11yTable";
 
 type Props = {
   componentName: string;
@@ -9,6 +11,10 @@ export const Persona1Table2 = ({ componentName, personaName }: Props) => {
   return (
     <>
       <Header heading={personaName} textContent={componentName} />
+
+      <Main>
+        <A11yTable />
+      </Main>
     </>
   );
 };
