ccd4253a65efbb05e15f5cc98928b6753fe8b040
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-10-23 11:20:47 +0200

    feat(BadNavExp.tsx): add text content

--- src/components/pages/persona1/persona1nav1/BadNavExp.tsx
+++ src/components/pages/persona1/persona1nav1/BadNavExp.tsx
@@ -1,6 +1,8 @@
 import { Heading2 } from "../../../UI/headings/Heading2";
+import { KeyBoardInput } from "../../../UI/keyboard-inputs/KeyboardInput";
 import { Paragraph } from "../../../UI/paragraph/Paragraph";
 import { Section } from "../../../UI/sections/Section";
+import { SrDisplay } from "../../../UI/sr-display/SrDisplay";
 import { HtmlTag } from "../../../UI/tags/HtmlTag";
 
 export const BadNavExp = () => {
@@ -9,14 +11,47 @@ export const BadNavExp = () => {
       <Heading2>Explications</Heading2>
       <Paragraph>
         Nous voyons que notre code utilise uniquement les balises{" "}
-        <HtmlTag tagName="a" /> comme balise sémantique, ce qui fait que les
-        technologies d’assistance ne reconnaitron pas notre menu de navigation
-        en tant que telle.
+        <HtmlTag tagName="a" /> comme balise sémantique, les liens sont donc
+        bien reconnus par les technologie d’assistance. Nous allons voir ce
+        qu’indique un lecteur d’écran.
       </Paragraph>
+
+      <SrDisplay>
+        <ol>
+          <li className="my-4">
+            <KeyBoardInput keyName="Tab" />
+            <span>Accueil lien</span>
+          </li>
+          <li className="my-4">
+            <KeyBoardInput keyName="Tab" />
+            <span>Produits lien</span>
+          </li>
+          <li className="my-4">
+            <KeyBoardInput keyName="Tab" />
+            <span>Services lien</span>
+          </li>
+          <li className="my-4">
+            <KeyBoardInput keyName="Tab" />
+            <span>Contact lien</span>
+          </li>
+        </ol>
+      </SrDisplay>
+
       <Paragraph>
-        De plus on ne pourra pas savoir qu’il s’agit d’une liste et donc
-        l’utilisateur de lecteur d’écran ne saura pas de combien de liens est
-        composés le menu.
+        Le lecteur d’écran ne lit donc que “lien” sans aucune autre informations
+        sémantique.
+      </Paragraph>
+
+      <Paragraph>
+        Les différentes technologie d’assistance dont le lecteur d’écran fait
+        partie permettent aux utilisateur d’aller directement au menu de
+        navigation, mais il faut que celui-ci soit identifié sémantiquement.
+      </Paragraph>
+
+      <Paragraph>
+        De même indiqué que chaque lien est un élément faisant partie d’une
+        liste peut aider les utilsateurs à mieux se situer dans le flux
+        d’information.
       </Paragraph>
     </Section>
   );
