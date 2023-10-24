f96ff0eba07bdd31d85db8d9d5288490c681a432
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-10-24 15:18:57 +0200

    feat(Heading1.tsx): add types & twMerge

--- src/components/UI/headings/Heading1.tsx
+++ src/components/UI/headings/Heading1.tsx
@@ -1,13 +1,18 @@
 import type { DetailedHTMLProps, HTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
 
 type Props = DetailedHTMLProps<
   HTMLAttributes<HTMLHeadingElement>,
   HTMLHeadingElement
 >;
 
-export const Heading1 = ({ children, ...rest }: Props) => {
+export const Heading1 = ({ children, className, ...rest }: Props) => {
+  const mergedClassName = twMerge(
+    "my-4 text-center text-4xl lg:text-5xl",
+    className,
+  );
   return (
-    <h1 className="my-4 text-center text-4xl lg:text-5xl" {...rest}>
+    <h1 className={mergedClassName} {...rest}>
       {children}
     </h1>
   );
