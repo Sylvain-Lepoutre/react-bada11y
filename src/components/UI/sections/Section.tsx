dca0e7e656e241a60e98818a8ad0a8d63ac7a2db
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-10-25 10:18:23 +0200

    feat(Section.tsx): use cn function

--- src/components/UI/sections/Section.tsx
+++ src/components/UI/sections/Section.tsx
@@ -1,16 +1,18 @@
 import type { DetailedHTMLProps, DetailsHTMLAttributes } from "react";
-import { twMerge } from "tailwind-merge";
+
+import { cn } from "../../../helpers/cn";
 
 type Props = DetailedHTMLProps<DetailsHTMLAttributes<HTMLElement>, HTMLElement>;
 
 export const Section = ({ children, className, ...rest }: Props) => {
-  const mergedClassName = twMerge(
-    "my-10 rounded-2xl bg-white p-4 shadow-Custom lg:px-8",
-    className,
-  );
-
   return (
-    <section className={mergedClassName} {...rest}>
+    <section
+      className={cn(
+        "my-10 rounded-2xl bg-white p-4 shadow-Custom lg:px-8",
+        className,
+      )}
+      {...rest}
+    >
       {children}
     </section>
   );
