7d4ba8dea26baa6136f025562ce7ea9ab5aab165
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-10-25 13:35:59 +0200

    feat(A11yNavExp.tsx): update content

--- src/components/pages/persona1/persona1nav2/A11yNavExp.tsx
+++ src/components/pages/persona1/persona1nav2/A11yNavExp.tsx
@@ -2,8 +2,11 @@ import { useId } from "react";
 
 import { Heading2 } from "../../../UI/headings/Heading2";
 import { KeyBoardInput } from "../../../UI/keyboard-inputs/KeyboardInput";
+import { Paragraph } from "../../../UI/paragraph/Paragraph";
 import { Section } from "../../../UI/sections/Section";
 import { SrDisplay } from "../../../UI/sr-display/SrDisplay";
+import { HtmlTag } from "../../../UI/tags/HtmlTag";
+import { InlineCode } from "../../../UI/tags/InlineCode";
 
 export const A11yNavExp = () => {
   const id = useId();
@@ -31,6 +34,21 @@ export const A11yNavExp = () => {
           </li>
         </ol>
       </SrDisplay>
+
+      <Paragraph>
+        Avec l’utilisation de la balise <HtmlTag tagName="nav" />, et de
+        l’attribut ARIA, <InlineCode code='role="navigation"' />, nous indiquons
+        correctement que nos lien de navigations sont dans un menu de
+        navigation, et les utilsateurs de technologie d’assistance pourront se
+        rendre directement à celui ci grâce aux commandes de ces outils.
+      </Paragraph>
+
+      <Paragraph>
+        Nous avons aussi utlisé les balises <HtmlTag tagName="ul" /> et
+        <HtmlTag tagName="li" /> afin d’indiquer que ce menu est une liste qui
+        comprend 4 éléments. Cela renforce la structure de notre page afin
+        d’assurer une meilleure expérience de navigation.
+      </Paragraph>
     </Section>
   );
 };
