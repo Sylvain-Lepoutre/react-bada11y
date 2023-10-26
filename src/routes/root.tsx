9df3cbd71bb574455edb68371e8895ad6a177ec0
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-10-26 16:28:47 +0200

    fix(project): file name

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -3,7 +3,7 @@ import { IntroSection } from "../components/pages/root/IntroSection";
 import { ObjectiveSection } from "../components/pages/root/ObjectiveSection";
 import { PersonaSection } from "../components/pages/root/PersonaSection";
 import { Main } from "../components/UI/main/Main";
-import { SkipLinks } from "../components/UI/skipLinks/SkipLinks";
+import { SkipLinks } from "../components/UI/skiplinks/SkipLinks";
 import { useDocumentTitle } from "../hooks/useDocumentTitle";
 
 export const Root = () => {
