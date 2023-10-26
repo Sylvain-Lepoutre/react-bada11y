14e9929317dfe87500285dd953aab80c1dc5f71c
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-10-26 15:05:37 +0200

    fieat(PersonaSection.tsx): add nav

--- src/components/pages/root/PersonaSection.tsx
+++ src/components/pages/root/PersonaSection.tsx
@@ -1,23 +1,27 @@
+import { useId } from "react";
 import { personas } from "../../../data/personas";
 import { Heading2 } from "../../UI/headings/Heading2";
 import { Section } from "../../UI/sections/Section";
 import { PersonaCard } from "./PersonaCard";
 
 export const PersonaSection = () => {
+  const id = useId();
+
   return (
-    <Section>
-      <Heading2> Choisissez Votre Persona de sensibilisation</Heading2>
-      {/* TODO mettre une nav ici ?  */}
-      <ul className="my-8 grid gap-12 text-center md:grid-cols-2 lg:my-12">
-        {personas.map((persona) => (
-          <PersonaCard
-            cardTitle={persona.cardTitle}
-            imgSrc={persona.imgSrc}
-            key={persona.id}
-            linkURL={persona.personaHomeUrl}
-          />
-        ))}
-      </ul>
+    <Section aria-labelledby={id}>
+      <Heading2 id={id}> Choisissez Votre Persona de sensibilisation</Heading2>
+      <nav>
+        <ul className="my-8 grid gap-12 text-center md:grid-cols-2 lg:my-12">
+          {personas.map((persona) => (
+            <PersonaCard
+              cardTitle={persona.cardTitle}
+              imgSrc={persona.imgSrc}
+              key={persona.id}
+              linkURL={persona.personaHomeUrl}
+            />
+          ))}
+        </ul>
+      </nav>
     </Section>
   );
 };
