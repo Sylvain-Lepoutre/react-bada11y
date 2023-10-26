a08cfcdb4354255cbf4cc5536e59c07dd6c406a3
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-10-26 16:13:56 +0200

    feat(Skiplinks.tsx): create & use component

--- src/components/UI/main/Main.tsx
+++ src/components/UI/main/Main.tsx
@@ -8,6 +8,7 @@ export const Main = ({ children, className, ...rest }: Props) => {
   return (
     <main
       className={cn("mx-auto my-12 w-11/12 lg:w-9/12", className)}
+      id="main"
       role="main"
       {...rest}
     >
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -3,12 +3,15 @@ import { IntroSection } from "../components/pages/root/IntroSection";
 import { ObjectiveSection } from "../components/pages/root/ObjectiveSection";
 import { PersonaSection } from "../components/pages/root/PersonaSection";
 import { Main } from "../components/UI/main/Main";
+import { SkipLinks } from "../components/UI/skiplinks/SkipLinks";
 import { useDocumentTitle } from "../hooks/useDocumentTitle";
 
 export const Root = () => {
   useDocumentTitle("Inotest - Découvrez l'inclusion en ligne");
   return (
     <>
+      <SkipLinks />
+
       <HeaderRoot />
 
       <Main>
