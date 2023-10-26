4ded7f9cec70e79ce0c64fc4ae9ccf8a54b5ae98
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-10-26 15:01:12 +0200

    feat(BadFont.tsx): update component

--- src/components/pages/persona4/persona4font1/BadFont.tsx
+++ src/components/pages/persona4/persona4font1/BadFont.tsx
@@ -1,10 +1,36 @@
+import { useId } from "react";
+
 import { Heading2 } from "../../../UI/headings/Heading2";
+import { Paragraph } from "../../../UI/paragraph/Paragraph";
 import { Section } from "../../../UI/sections/Section";
 
 export const BadFont = () => {
+  const id = useId();
+
   return (
-    <Section>
-      <Heading2>Affichage dans le navigateur</Heading2>
+    <Section aria-labelledby={id}>
+      <Heading2 id={id}>Affichage dans le navigateur</Heading2>
+
+      <Paragraph className="great-vibes">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
+        asperiores adipisci fuga porro quia consequatur beatae quas aperiam
+        repellat. Id sint magni a eum possimus voluptates quae temporibus
+        repellat enim!
+      </Paragraph>
+
+      <Paragraph className="courgette">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
+        asperiores adipisci fuga porro quia consequatur beatae quas aperiam
+        repellat. Id sint magni a eum possimus voluptates quae temporibus
+        repellat enim!
+      </Paragraph>
+
+      <Paragraph className="spectral-sc">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
+        asperiores adipisci fuga porro quia consequatur beatae quas aperiam
+        repellat. Id sint magni a eum possimus voluptates quae temporibus
+        repellat enim!
+      </Paragraph>
     </Section>
   );
 };
