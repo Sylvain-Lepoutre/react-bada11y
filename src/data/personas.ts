88064f84f698350069707e2f5f18e1d9cc849abb
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-10-27 11:36:05 +0200

    fix(persona.ts): update HomeUrl

--- src/data/personas.ts
+++ src/data/personas.ts
@@ -26,7 +26,7 @@ export const personas: Persona[] = [
     name: "Djebrine",
     cardTitle: "Djebrine, 33 ans, entrepreneur, trouble visuel",
     imgSrc: "https://placehold.co/128x128/png",
-    personaHomeUrl: "/persona1",
+    personaHomeUrl: "/persona/1",
     intro:
       "Djebrine est un entrepreneur dynamique de 33 ans. Il est non-voyant. Chaque matin, il s’installe devant son ordinateur, équipé d’un lecteur d’écran et d’un afficheur braille. Le lecteur d’écran est un logiciel doté d’un synthétiseur vocal qui récupere l’information textuelle et la retranscrit vocalement et en braille pour Djebrine. Il traite autant le texte saisi au clavier que le contenu de l'écran (menus, boîtes de dialogue, texte des documents, pages Internet…). Ces outils technologiques lui servent de pont entre son monde sans vision et le monde numérique.",
     steps: [
@@ -61,7 +61,7 @@ export const personas: Persona[] = [
     name: "Sophie",
     cardTitle: "Sophie, 42 ans, comptable, trouble moteur",
     imgSrc: "https://placehold.co/128x128/png",
-    personaHomeUrl: "/persona2",
+    personaHomeUrl: "/persona/2",
     intro:
       "Sophie est une comptable de 42 ans. Elle lutte contre de sévères troubles musculo-squelettiques. Ses douleurs l'empeche d'utiliser une souris, elle navigue à l'aide du clavier.",
     steps: [
@@ -84,7 +84,7 @@ export const personas: Persona[] = [
     name: "Amine",
     cardTitle: "Amine, 23 ans, étudiant, trouble auditif",
     imgSrc: "https://placehold.co/128x128/png",
-    personaHomeUrl: "/persona3",
+    personaHomeUrl: "/persona/3",
     intro:
       "Amine est un jeune homme de 23 ans, étudiant en Master dans ce domaine. Il est sourd depuis l'âge d'un an à la suite d'une méningite. Il aime regarder des films en ligne, cependant les sous-titres ne sont pas toujours présents ou sont mal syncrhonisés, voire incomplets, cela rend son expérience frustrante et désagréable.",
     steps: [
@@ -100,7 +100,7 @@ export const personas: Persona[] = [
     name: "Claire",
     cardTitle: "Claire, 30 ans, professeur, trouble cognitif",
     imgSrc: "https://placehold.co/128x128/png",
-    personaHomeUrl: "/persona4",
+    personaHomeUrl: "/persona/4",
     intro:
       "Claire est une professeure de yoga de 33 ans. Elle est dyslexique et a un trouble du déficit de l'attention avec hyperactivité. Elle s’épanouit par le yoga, mais le monde numérique reste un défi pour elle. Sa dyslexie la rend réticente face aux textes long, et elle a du mal à maintenir sa concentraion.",
     steps: [
