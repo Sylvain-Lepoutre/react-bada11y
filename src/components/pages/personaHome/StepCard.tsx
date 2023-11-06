1201fad6e0d4f2adaeafc33b323d36cb97ec0a63
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-11-06 08:34:56 +0100

    fix(project): clean files

--- src/components/pages/personaHome/StepCard.tsx
+++ src/components/pages/personaHome/StepCard.tsx
@@ -3,8 +3,6 @@ import type { Step } from "../../../data/personas";
 type Props = Step;
 
 export const StepCard = ({ name, pages }: Props) => {
-  console.log(pages);
-
   return (
     <li className="mx-auto  w-9/12 max-w-xs list-none  rounded-2xl p-4 shadow-CustomCard outline outline-2 outline-BluePastel  hover:shadow-CustomCardHover hover:-outline-offset-8">
       <a
