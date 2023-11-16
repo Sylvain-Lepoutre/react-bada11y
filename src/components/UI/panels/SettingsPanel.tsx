662a1839957ae2d56ec2e6286eb7233537423310
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-11-16 13:57:46 +0100

    feat(SettingsPannel.tsx): create Conformance section

--- src/components/UI/panels/SettingsPanel.tsx
+++ src/components/UI/panels/SettingsPanel.tsx
@@ -1,18 +1,53 @@
+import { useState, type ChangeEvent } from "react";
+
 import { ButtonLink } from "~/components/UI";
 import { personas } from "~/data/personas";
 
 export const SettingsPanel = () => {
+  const [conformanceLevel, setConformanceLevel] = useState("");
+
+  const handleChangeConformanceLevel = (
+    event: ChangeEvent<HTMLInputElement>,
+  ) => {
+    setConformanceLevel(event.target.value);
+  };
+
+  console.log(conformanceLevel);
+
   return (
     <div className="flex flex-col gap-8">
       <header>InoTest</header>
 
       <section>
         <h2>Niveau de conformité</h2>
+        <div className="flex flex-col">
+          <div>
+            <input
+              checked={conformanceLevel === "levelA"}
+              id="levelA"
+              name="conformance"
+              onChange={handleChangeConformanceLevel}
+              type="radio"
+              value="levelA"
+            />
+            <label htmlFor="levelA">Niveau A</label>
+          </div>
 
-        <ul>
-          <li>A (?)</li>
-          <li>AA (?)</li>
-        </ul>
+          <div>
+            <input
+              checked={conformanceLevel === "levelAA"}
+              id="levelAA"
+              name="conformance"
+              onChange={handleChangeConformanceLevel}
+              type="radio"
+              value="levelAA"
+            />
+            <label htmlFor="levelAA">Niveau AA</label>
+          </div>
+        </div>
+
+        {conformanceLevel === "levelA" && <h2>TEST niveau A</h2>}
+        {conformanceLevel === "levelAA" && <h2>TEST niveau AA</h2>}
       </section>
 
       <section>
